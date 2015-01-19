ecba037a4974253bc7f740951d744e59da5c3f7b
Ivan Choo <ivanchoo.private@gmail.com>
2015-01-19 17:09:00 +0800

    Initial Commit

---
+++ MonthCalendarView/MonthCalendarView.h
@@ -0,0 +1,19 @@
+//
+//  MonthCalendarView.h
+//  MonthCalendarView
+//
+//  Created by Ivan Choo on 19/1/15.
+//  Copyright (c) 2015 Ivan Choo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MonthCalendarView.
+FOUNDATION_EXPORT double MonthCalendarViewVersionNumber;
+
+//! Project version string for MonthCalendarView.
+FOUNDATION_EXPORT const unsigned char MonthCalendarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MonthCalendarView/PublicHeader.h>
+
+
